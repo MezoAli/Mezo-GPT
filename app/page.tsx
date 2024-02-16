fdb7504ab56a4dbea03659873bdadc5b17308a74
Moutaz Ali <moutaz.ali.dev@gmail.com>
2024-02-16 21:52:02 +0200

    setting up clerk authentication and protect all routes except the home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+// import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
@@ -13,6 +14,7 @@ export default function Home() {
           <Link href="/chat" className="btn btn-secondary">
             Get Started
           </Link>
+          {/* <UserButton /> */}
         </div>
       </div>
     </div>
