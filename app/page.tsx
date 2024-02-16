6657ecf4ed5fe36742f7076ac26302ca3f287b6b
Moutaz Ali <moutaz.ali.dev@gmail.com>
2024-02-16 21:38:09 +0200

    create home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,20 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <>
-      <h1 className="text-center bg-black text-white py-4">Hello Mezo</h1>
-      <button className="btn btn-primary">Click Me</button>
-    </>
+    <div className="hero min-h-screen bg-base-200">
+      <div className="hero-content text-center">
+        <div className="max-w-md">
+          <h1 className="text-5xl font-bold text-primary">Mezo-GPT</h1>
+          <p className="py-6 capitalize text-2xl leading-loose">
+            chat app like Chat-gpt and tour gide generator to specify where you
+            need to go and let us decide the beautiful places to visit
+          </p>
+          <Link href="/chat" className="btn btn-secondary">
+            Get Started
+          </Link>
+        </div>
+      </div>
+    </div>
   );
 }
