d3df9273358543c4f9827be37edbb969e00bfe89
Moutaz Ali <moutaz.ali.dev@gmail.com>
2024-02-17 13:21:13 +0200

    adding sidebar to layout

---
+++ components/Sidebar.tsx
@@ -0,0 +1,15 @@
+import NavLinks from "./NavLinks";
+import SidebarHeader from "./SidebarHeader";
+import UserProfile from "./UserProfile";
+
+const Sidebar = () => {
+  return (
+    <div className="w-80 min-h-full bg-base-300 px-4 py-12 grid grid-rows-[auto,1fr,auto]">
+      <SidebarHeader />
+      <NavLinks />
+      <UserProfile />
+    </div>
+  );
+};
+
+export default Sidebar;
