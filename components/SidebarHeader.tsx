91a24a970de1a3d2f1a0c139b0fb95073c958b67
Moutaz Ali <moutaz.ali.dev@gmail.com>
2024-02-17 14:51:56 +0200

    activiting theme toggle button

--- components/SidebarHeader.tsx
+++ components/SidebarHeader.tsx
@@ -3,7 +3,7 @@ import { SiOpenaigym } from "react-icons/si";
 
 const SidebarHeader = () => {
   return (
-    <div className="flex items-center mb-4 gap-4 px-4">
+    <div className="flex items-center justify-between w-full mb-6 gap-4 px-4">
       <SiOpenaigym className="w-10 h-10 text-primary font-bold" />
       <h2 className="text-2xl font-extrabold">MezoGPT</h2>
       <ToggleTheme />
--- components/ToggleTheme.tsx
+++ components/ToggleTheme.tsx
@@ -1,5 +1,25 @@
+"use client";
+import { useState } from "react";
+import { BsMoonFill, BsSunFill } from "react-icons/bs";
+
 const ToggleTheme = () => {
-  return <button className="btn btn-primary btn-sm">ToggleTheme</button>;
+  const [theme, setTheme] = useState("winter");
+  const handleThemeToggle = () => {
+    setTheme((prevValue: string) => {
+      const value = prevValue === "winter" ? "night" : "winter";
+      document.documentElement.setAttribute("data-theme", value);
+      return value;
+    });
+  };
+  return (
+    <button className="btn btn-outline" onClick={handleThemeToggle}>
+      {theme === "winter" ? (
+        <BsMoonFill className="w-5 h-5" />
+      ) : (
+        <BsSunFill className="w-5 h-5 text-yellow-500" />
+      )}
+    </button>
+  );
 };
 
 export default ToggleTheme;
