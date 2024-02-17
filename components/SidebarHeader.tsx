bafe42fbe5c258e22b5560675e16ee7eddb8df94
Moutaz Ali <moutaz.ali.dev@gmail.com>
2024-02-17 13:26:25 +0200

    create sidebar header with it's components

--- components/SidebarHeader.tsx
+++ components/SidebarHeader.tsx
@@ -1,5 +1,14 @@
+import ToggleTheme from "./ToggleTheme";
+import { SiOpenaigym } from "react-icons/si";
+
 const SidebarHeader = () => {
-  return <div>SidebarHeader</div>;
+  return (
+    <div className="flex items-center mb-4 gap-4 px-4">
+      <SiOpenaigym className="w-10 h-10 text-primary font-bold" />
+      <h2 className="text-2xl font-extrabold">MezoGPT</h2>
+      <ToggleTheme />
+    </div>
+  );
 };
 
 export default SidebarHeader;
---
+++ components/ToggleTheme.tsx
@@ -0,0 +1,5 @@
+const ToggleTheme = () => {
+  return <button className="btn btn-primary btn-sm">ToggleTheme</button>;
+};
+
+export default ToggleTheme;
