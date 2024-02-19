e4e36560cef7e2bb3476199bcdfb68c6e10e82fd
Moutaz Ali <moutaz.ali.dev@gmail.com>
2024-02-19 19:01:32 +0200

    create new tour page and form

---
+++ components/NewTour.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import { FormEvent } from "react";
+
+const NewTour = () => {
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+    console.log(Object.fromEntries(formData.entries()));
+  };
+  return (
+    <>
+      <div className="max-w-3xl">
+        <p className="mb-4 capitalize text-xl">select your dream destination</p>
+        <form className="w-full" onSubmit={handleSubmit}>
+          <div className="join">
+            <input
+              className="input input-bordered join-item w-full"
+              type="text"
+              placeholder="City"
+              name="city"
+              required
+            />
+            <input
+              className="input input-bordered join-item w-full"
+              type="text"
+              placeholder="Country"
+              name="country"
+              required
+            />
+            <button className="btn btn-primary">Submit</button>
+          </div>
+        </form>
+      </div>
+      <div className="mt-16">Tour Info</div>
+    </>
+  );
+};
+
+export default NewTour;
