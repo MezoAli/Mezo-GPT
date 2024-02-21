e4353c280963bf19692487092c3fac85a8c6ecdc
Moutaz Ali <moutaz.ali.dev@gmail.com>
2024-02-21 23:25:12 +0200

    get all tours and display all existing tours

--- components/ToursList.tsx
+++ components/ToursList.tsx
@@ -1,5 +1,13 @@
-const ToursList = () => {
-  return <div>ToursList</div>;
+import TourCard from "./TourCard";
+
+const ToursList = ({ tours }: any) => {
+  return (
+    <div className="grid sm:grid-cols-2  lg:grid-cols-4 gap-8">
+      {tours?.map((tour: any) => {
+        return <TourCard key={tour.id} tour={tour} />;
+      })}
+    </div>
+  );
 };
 
 export default ToursList;
--- components/ToursPage.tsx
+++ components/ToursPage.tsx
@@ -1,5 +1,19 @@
+"use client";
+
+import { getAllTours } from "@/utils/actions";
+import { useQuery } from "@tanstack/react-query";
+import ToursList from "./ToursList";
+
 const ToursPage = () => {
-  return <div>ToursPage</div>;
+  const { data: tours } = useQuery({
+    queryKey: ["tours"],
+    queryFn: () => getAllTours(),
+  });
+
+  if (tours?.length === 0) {
+    return <p className="text-center text-2xl ">NO tours found...</p>;
+  }
+  return <ToursList tours={tours} />;
 };
 
 export default ToursPage;
