670d13039a1a033f9a155a61393ad809b8cf2b37
Moutaz Ali <moutaz.ali.dev@gmail.com>
2024-02-24 19:07:07 +0200

    display users tokens in profile page

--- app/(dashboard)/profile/page.tsx
+++ app/(dashboard)/profile/page.tsx
@@ -1,8 +1,14 @@
-import { UserProfile } from "@clerk/nextjs";
+import { fetchOrgenerateTokens } from "@/utils/actions";
+import { UserProfile, auth } from "@clerk/nextjs";
 
-const ProfilePage = () => {
+const ProfilePage = async () => {
+  const { userId } = auth();
+  const tokens = await fetchOrgenerateTokens(userId as string);
   return (
-    <div className="w-full flex justify-center items-start">
+    <div className="w-full flex justify-center items-start flex-col gap-4">
+      <p className="mb-8 font-extrabold text-2xl text-center w-full">
+        Tokens : {tokens}
+      </p>
       <UserProfile />
     </div>
   );
